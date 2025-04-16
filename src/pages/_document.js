c6a846324666bc420b983f3032e3c79690463368
Blunistark <meghnath1782004@gmail.com>
2025-04-16 21:59:29 +0530

    test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
       </body>
     </Html>
   );
